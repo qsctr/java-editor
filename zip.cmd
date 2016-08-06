affadcfe9623e647c2cb05661e74bff14142c282
qsctr <brettonchen@gmail.com>
2016-08-06 16:51:39 +0800

    Save space by deleting unused ace builds on zip

--- zip.cmd
+++ zip.cmd
@@ -1,1 +1,7 @@
+rem save 23.84 MB of space, 13.8% of uncompressed version
+rem 4.3 MB of space, 7.1% of compressed version
+rmdir /S /Q "Java Editor-win32-x64/resources/app/node_modules/ace-builds/src"
+rmdir /S /Q "Java Editor-win32-x64/resources/app/node_modules/ace-builds/src-min"
+rmdir /S /Q "Java Editor-win32-x64/resources/app/node_modules/ace-builds/src-min-noconflict"
+
 jar -cMf "Java-Editor.zip" "Java Editor-win32-x64"
