a7cd491a39fa7471c7279225bcf8e934ee215c1e
qsctr <brettonchen@gmail.com>
2016-08-06 19:25:31 +0800

    Remove zip file if it exists to save 58MB of space

--- zip.cmd
+++ zip.cmd
@@ -1,4 +1,5 @@
-rem save 23.84 MB of space uncompressed, 4.3 MB of space compressed
+rem save space
+if exist "Java Editor-win32-x64/resources/app/Java-Editor.zip" rmdir /S /Q "Java Editor-win32-x64/resources/app/Java-Editor.zip"
 rmdir /S /Q "Java Editor-win32-x64/resources/app/node_modules/ace-builds/src"
 rmdir /S /Q "Java Editor-win32-x64/resources/app/node_modules/ace-builds/src-min"
 rmdir /S /Q "Java Editor-win32-x64/resources/app/node_modules/ace-builds/src-min-noconflict"
