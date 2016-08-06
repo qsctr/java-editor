e322f152777da8d7ab6f1d90adfc8807e7dd662c
qsctr <brettonchen@gmail.com>
2016-08-06 19:14:40 +0800

    Save 5MB by removing devDependencies from build

--- zip.cmd
+++ zip.cmd
@@ -1,5 +1,4 @@
-rem save 23.84 MB of space, 13.8% of uncompressed version
-rem 4.3 MB of space, 7.1% of compressed version
+rem save 23.84 MB of space uncompressed, 4.3 MB of space compressed
 rmdir /S /Q "Java Editor-win32-x64/resources/app/node_modules/ace-builds/src"
 rmdir /S /Q "Java Editor-win32-x64/resources/app/node_modules/ace-builds/src-min"
 rmdir /S /Q "Java Editor-win32-x64/resources/app/node_modules/ace-builds/src-min-noconflict"
