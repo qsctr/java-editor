f4ddc62e660d7a3b6840851c47339ab9fa3ddd38
qsctr <brettonchen@gmail.com>
2016-08-09 14:18:12 +0800

    Edit HTMLCollectionSymbolIterator.d.ts

--- declarations/HTMLCollectionSymbolIterator.d.ts
+++ declarations/HTMLCollectionSymbolIterator.d.ts
@@ -1,7 +1,6 @@
 interface HTMLCollection {
     /**
      * The iterator for HTMLCollection.
-     * This is implemented in Chrome 51.
      */
     [Symbol.iterator](): IterableIterator<Element>;
 }
