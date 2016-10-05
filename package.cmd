42bbf465da7c6646c643986e3bc03f4416bb458e
qsctr <brettonchen@gmail.com>
2016-10-05 23:30:04 +0800

    Specify version, new packager cli in package.cmd

--- package.cmd
+++ package.cmd
@@ -1,14 +1,17 @@
+@if [%1] == [] (
+    @echo you must give an argument which is the version number
+    goto :eof
+)
+
 call compile
 
-call electron-packager . --overwrite --platform=win32 --arch=x64 --prune --version-string.CompanyName="" --version-string.FileDescription="Java Editor" --icon="icon.ico"
+for /f "delims=" %%v in ('node -p "require('./package.json').version"') do set current_version=%%v
 
-rem save space
-cd Java Editor-win32-x64/resources/app
-if exist Java-Editor.zip del Java-Editor.zip
-cd node_modules/ace-builds
-if exist src/ rmdir /S /Q src
-if exist src-min/ rmdir /S /Q src-min
-if exist src-noconflict/ rmdir /S /Q src-noconflict
-cd ../../../../..
+if not [%1] == [%current_version%] (
+    call npm --no-git-tag-version version %1
+    @echo on
+)
+
+call electron-packager . --platform=win32 --arch=x64 --app-copyright="(c) 2016 Bretton" --build-version="%1" --icon="icon.ico" --ignore="Java-Editor.zip|node_modules/ace-builds/src$|node_modules/ace-builds/src-min$|node_modules/ace-builds/src-noconflict" --overwrite --win32metadata.CompanyName="" --win32metadata.FileDescription="Java Editor" --win32metadata.OriginalFileName="" --win32metadata.ProductName="Java Editor" --win32metadata.InternalName=""
 
 jar -cMf Java-Editor.zip "Java Editor-win32-x64"
