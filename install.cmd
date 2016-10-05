e793d0b26a51a25e2ec92ad8286d7d34b0ddc702
qsctr <brettonchen@gmail.com>
2016-10-05 23:27:40 +0800

    Update electron and electron-packager versions

--- install.cmd
+++ install.cmd
@@ -2,5 +2,5 @@ npm install
 npm install -g typescript@2.0.3
 npm install -g typings@1.4.0
 typings install
-npm install -g electron-packager@7.4.0
+npm install -g electron-packager@8.1.0
 call compile
